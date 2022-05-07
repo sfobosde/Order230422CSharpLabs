10f99c8cd7891cbe0e313e67cc739f96d42656f1
Grigory Vasilev <grishavasilev62@gmail.com>
2022-05-08 00:47:05 +0300

    realize squad class

---
+++ Option1/LR9/Squad.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace LR9
+{
+	/// <summary>
+	/// Класс - квадрат.
+	/// </summary>
+	class Squad
+	{
+		#region Свойства
+		/// <summary>
+		/// Длина стороны
+		/// </summary>
+		public double SideLength
+		{
+			get
+			{
+				return sideLength;
+			}
+			set
+			{
+				/// Проверка на неотрицательность значений.
+				if (value>0)
+				{
+					sideLength = value;
+
+					/// Перерассчитываем все свойства квадрата.
+					CalculateOptions();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Диагональ квадрата.
+		/// </summary>
+		public double Diagonal
+		{
+			/// Только возвращаем значение длины диагонали.
+			get
+			{
+				return diagonal;
+			}
+		}
+
+		/// <summary>
+		/// Периметр квадрата.
+		/// </summary>
+		public double Perimetr
+		{
+			/// Только возвращаем значение периметра.
+			get
+			{
+				return perimetr;
+			}
+		}
+
+		/// <summary>
+		/// Площадь квадрата.
+		/// </summary>
+		public double Square
+		{
+			/// Только возвращаем значение периметра.
+			get
+			{
+				return square;
+			}
+		}
+		#endregion
+
+		#region Поля
+		/// <summary>
+		/// Длина стороны.
+		/// </summary>
+		private double sideLength;
+
+		/// <summary>
+		/// Диагональ квадрата.
+		/// </summary>
+		private double diagonal;
+
+		/// <summary>
+		/// Периметр квадрата.
+		/// </summary>
+		private double perimetr;
+
+		/// <summary>
+		/// Площадь квадрата.
+		/// </summary>
+		private double square;
+		#endregion
+
+		#region Конструкторы
+		/// <summary>
+		/// В конструкторе передаем длину стороны.
+		/// </summary>
+		/// <param name="sideLength"></param>
+		public Squad(double sideLength)
+		{
+			this.sideLength = sideLength;
+
+			/// Рассчитываем все свойства квадрата.
+			CalculateOptions();
+		}
+		#endregion
+
+		#region Методы
+		/// <summary>
+		/// Вычисление диагонали, площади, периметра.
+		/// </summary>
+		private void CalculateOptions()
+		{
+			/// Вычисление длины диагонали квадрата.
+			diagonal = Math.Sqrt(2*sideLength*sideLength);
+
+			/// Вычисляем периметр квадрата.
+			perimetr = sideLength * 4;
+
+			/// Вычисляем площадь квадрата.
+			square = sideLength * sideLength;
+		}
+		#endregion 
+	}
+}
