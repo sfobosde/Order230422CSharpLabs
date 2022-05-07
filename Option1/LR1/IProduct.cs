a755ffe2cda7258287ff349c3df0a0ca0399c64e
Grigory Vasilev <grishavasilev62@gmail.com>
2022-05-07 23:45:52 +0300

    Add interface

---
+++ Option1/LR1/IProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LR6
+{
+	/// <summary>
+	/// Интерфейс акции для класса Товара.
+	/// </summary>
+	interface IStock
+	{
+		/// <summary>
+		/// Использовать бонусы для оплаты товара.
+		/// </summary>
+		/// <param name="TotalPoints"></param>
+		void UsePointsToPay(int TotalPoints);
+
+		/// <summary>
+		/// Получить информацию о акции на данный товар.
+		/// </summary>
+		void GetStocks();
+
+		/// <summary>
+		/// Добавить акциюю на товар.
+		/// </summary>
+		/// <param name="StockInfo"></param>
+		void AddStock(string StockInfo);
+	}
+}
