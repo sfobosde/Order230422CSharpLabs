547921c7f67bf4cc4c02fc30b3fdc41aba253e0a
Grigory Vasilev <grishavasilev62@gmail.com>
2022-05-07 22:07:08 +0300

    Resolve 1-3

--- Option1/LR1/Product.cs
+++ Option1/LR1/Product.cs
@@ -21,5 +21,41 @@ namespace LR1
 		/// Стоимость.
 		/// </summary>
 		public float Price;
+
+		/// <summary>
+		/// Описание товара.
+		/// Дополнительное поле.
+		/// </summary>
+		public string Description;
+
+		/// <summary>
+		/// Оценка товара.
+		/// Дополнительное поле.
+		/// </summary>
+		public float Grade;
+
+		/// <summary>
+		/// Конструктор по умолчанию.
+		/// </summary>
+		public Product()
+		{
+			Name = "Без названия";
+			Count = 0;
+			Price = 0;
+			Description = "Нет описания товара";
+			Grade = 0;
+		}
+
+		/// <summary>
+		/// Конструктор с аргументами.
+		/// </summary>
+		public Product(string Name, int Count, float Price, string Description, float Grade)
+		{
+			this.Name = Name;
+			this.Count = Count;
+			this.Price = Price;
+			this.Description = Description;
+			this.Grade = Grade;
+		}
 	}
 }
