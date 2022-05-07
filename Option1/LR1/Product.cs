884d577a0832200f9109e573bb631dde202a9d06
Grigory Vasilev <grishavasilev62@gmail.com>
2022-05-07 22:01:53 +0300

    Add Product class

---
+++ Option1/LR1/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LR1
+{
+	/// <summary>
+	/// Класс товара.
+	/// </summary>
+	class Product
+	{
+		/// <summary>
+		/// Имя товара.
+		/// </summary>
+		public string Name;
+
+		/// <summary>
+		/// Колличество.
+		/// </summary>
+		public int Count;
+
+		/// <summary>
+		/// Стоимость.
+		/// </summary>
+		public float Price;
+	}
+}
