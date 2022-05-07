a1ce7f362e8e2b7284d51ae09442e485683f5b19
Grigory Vasilev <grishavasilev62@gmail.com>
2022-05-07 22:15:12 +0300

    Resolve 4

--- Option1/LR1/Program.cs
+++ Option1/LR1/Program.cs
@@ -6,7 +6,24 @@ namespace LR1
 	{
 		static void Main(string[] args)
 		{
+			Product emptyProduct = new Product();
+			Product table = new Product("Стол письменный", 20, 3000, "Для учебы и работы", (float)4.5);
 
+			PrintProductDetails(emptyProduct);
+			PrintProductDetails(table);
+		}
+
+		/// <summary>
+		/// Функция для вывода данных о товаре.
+		/// </summary>
+		/// <param name="product"></param>
+		static void PrintProductDetails(Product product)
+		{
+			Console.WriteLine($"Наименование товара:{product.Name}.");
+			Console.WriteLine($"Количество товара:{product.Count} штук.");
+			Console.WriteLine($"Стоимость товара:{product.Price} руб.");
+			Console.WriteLine($"Описание товара:{product.Description}.");
+			Console.WriteLine($"Оценка товара:{product.Grade}/5,0");
 		}
 	}
 }
