52f8e40972fb948680a3eafb763abb85d4f192e8
Grigory Vasilev <grishavasilev62@gmail.com>
2022-05-08 00:38:08 +0300

    add Square

---
+++ Option1/LR9/Square.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace LR9
+{
+	/// <summary>
+	/// Класс - квадрат.
+	/// </summary>
+	class Squad
+	{
+		#region Свойства
+		/// <summary>
+		/// Длина стороны
+		/// </summary>
+		public int SideLength
+		{
+			get
+			{
+				return sideLength;
+			}
+			set
+			{
+				/// Проверка на неотрицательность значений.
+				if (value>0)
+				{
+					sideLength = value;
+				}
+			}
+		}
+		#endregion
+
+		#region Поля
+		/// <summary>
+		/// Длина стороны.
+		/// </summary>
+		private int sideLength;
+		#endregion
+
+		#region Конструкторы
+		/// <summary>
+		/// В конструкторе передаем длину стороны.
+		/// </summary>
+		/// <param name="sideLength"></param>
+		public Squad(int sideLength)
+		{
+			this.sideLength = sideLength;
+		}
+		#endregion
+	}
+}
