844e3f96780fa68a9d4eb6de0e64202350119a53
Grigory Vasilev <grishavasilev62@gmail.com>
2022-05-07 22:59:45 +0300

    Add child classes

---
+++ Option1/LR1/Cosmetics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace LR6
+{
+	/// <summary>
+	/// Класс косметики.
+	/// Дочерний класс от Product.
+	/// </summary>
+	class Cosmetics : Product
+	{
+	}
+}
---
+++ Option1/LR1/Furniture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace LR6
+{
+	/// <summary>
+	/// Класс мебели.
+	/// Дочерний класс от Product.
+	/// </summary>
+	class Furniture : Product
+	{
+	}
+}
