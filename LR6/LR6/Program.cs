00ddc54b3043fcdb3156b605b7b4885c95d65846
Grigory Vasilev <grishavasilev62@gmail.com>
2022-05-08 05:48:35 +0300

    Resolve

--- LR6/LR6/Program.cs
+++ LR6/LR6/Program.cs
@@ -6,7 +6,11 @@ namespace LR6
 	{
 		static void Main(string[] args)
 		{
-			
+			Person person = new Person();
+			person.GetPersonData();
+
+			person = new Person("Иван",	"Иванов", 18,true, "89654728651");
+			person.GetPersonData();
 		}
 	}
 }
