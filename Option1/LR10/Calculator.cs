0d8bd9e9dc25e0c783a1ef696c26e957b089ebcd
Grigory Vasilev <grishavasilev62@gmail.com>
2022-05-08 03:25:45 +0300

    Addition func

--- Option1/LR10/Calculator.cs
+++ Option1/LR10/Calculator.cs
@@ -7,5 +7,15 @@ namespace LR10
 	/// </summary>
 	class Calculator
 	{
+		/// <summary>
+		/// Сложение.
+		/// </summary>
+		/// <param name="firstArg"></param>
+		/// <param name="secondArg"></param>
+		/// <returns></returns>
+		public double Addition(double firstArg, double secondArg)
+		{
+			return firstArg + secondArg;
+		}
 	}
 }
